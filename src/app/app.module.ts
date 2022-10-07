9799595da383e4bf3a6445143880660b6d2e7539
MadanMohanReddy <madan.patakota@hotmail.com>
2022-10-07 08:34:12 +0530

    Basic Setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,33 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { CustomersComponent } from './customers/customers.component';
+import { CustomerDetailsComponent } from './customer-details/customer-details.component';
+
+// const routes : Routes =
+// {home path ---> home component}         -- Route
+// {customes path --> customes component}  -- Route
+// {produts path --> products comopnent}   -- Route
+
+const appRoutes: Routes = [
+  {
+    path: 'main',   //http://localhost:4200/main
+    component: MainComponent,
+  },
+  {
+    path: 'customers',  //http://localhost:4200/customers
+    component: CustomersComponent,
+  },
+  ////http://localhost:4200/customerdetails
+  { path: 'customerdetails', component: CustomerDetailsComponent },
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
